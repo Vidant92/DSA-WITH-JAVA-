7c3775195c5914454125e04f6a28334fac91a9c4
Vidant Bhardwaj <vidantbhardwaj92@gmail.com>
2026-04-22 13:35:29 +0530

    add

--- src/queues/ReverseQueue.java
+++ src/queues/ReverseQueue.java
@@ -1,20 +1,43 @@
 package Queues;
+
 import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
 public class ReverseQueue {
+
     public static void main(String[] args) {
 
+        Queue<Integer> q = new LinkedList<>();
+
+        // add elements
+        q.add(10);
+        q.add(20);
+        q.add(30);
+        q.add(40);
+        q.add(50);
+
+        System.out.println("Original Queue: " + q);
+
+        ReverseQueue obj = new ReverseQueue();
+        q = obj.reverseQueue(q);
+
+        System.out.println("Reversed Queue: " + q);
     }
+
     public Queue<Integer> reverseQueue(Queue<Integer> q) {
         Stack<Integer> st = new Stack<>();
-        while(q.size()>0){
+
+        // queue se stack
+        while(!q.isEmpty()){
             st.push(q.remove());
         }
-        while(st.size()>0){
+
+        // stack se queue
+        while(!st.isEmpty()){
             q.add(st.pop());
         }
+
         return q;
     }
 }
